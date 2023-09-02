c27bac91bfede3a679bee1c30bcb8ec5622ccce2
Brayan Gonzalez Marchena <brgmarchena@gmail.com>
2023-09-01 20:01:31 -0600

    feat: add sepolia

--- api/hardhat.config.ts
+++ api/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: `${process.env.SEPOLIA_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
